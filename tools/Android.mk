f53a9c16d92f866d7e7967d0aa275f81e1d1bc5b
maxwen <max.weninger@gmail.com>
2017-12-27 17:37:03 +0100

    BT: add build guard for tools
    Change-Id: I752409500e3c429ed8c996abf05db5a4e9906c17

--- tools/Android.mk
+++ tools/Android.mk
@@ -1,4 +1,6 @@
 ifeq ($(BOARD_HAVE_BLUETOOTH_QCOM),true)
+ifeq ($(BOARD_USES_BLUETOOTH_QCOM_TOOLS),true)
 LOCAL_PATH:= $(call my-dir)
 include $(call all-makefiles-under,$(LOCAL_PATH))
+endif
 endif # BOARD_HAVE_BLUETOOTH_QCOM
